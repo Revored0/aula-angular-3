7bff303d10bb6ade083fcabd47c62b29bc34fe1f
João Revoredo <augustobatista48@gmail.com>
2025-03-12 22:01:48 -0300

    projeto pronto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './produto/cadastrar/cadastrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {
+    path:'cadastrar-produto',component:CadastrarComponent
+  }
+
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/produto/produto.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    CadastrarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
+  ]
+})
+export class ProdutoModule { }
